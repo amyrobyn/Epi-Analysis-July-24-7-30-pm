d24841587a4db2bad80a039f5200c6e7ce4cab6a
amyrobyn <amykrystosik@gmail.com>
2017-12-29 16:55:48 -0800

    sat scan is working
    compare the acute tested
    compare the confirmed acute pos vs neg

--- epi-new-story-dec-2017.R
+++ epi-new-story-dec-2017.R
@@ -1,8 +1,9 @@
+install.packages("proj4")
 #We need real time clinical and vector surveillance data to prevent arboviral outbreaks: 
 #a retrospective study of Zika, Chikungunya, and Dengue outbreaks in Cali Colombia 2014-2016
 
 library(haven)
-ds <- read_stata("C:\\Users\\amykr\\Box Sync\\Amy Krystosik's Files\\cali- epi of arboviruses\\cali.sivigila.arbovirus.2014-2016.dta")
+ds <- read_stata("C://Users//amykr//Box Sync//Amy Krystosik's Files//cali- epi of arboviruses//cali.sivigila.arbovirus.2014-2016.dta")
 table(ds$nom_eve, ds$cod_eve)
 
 # lab results -------------------------------------------------------------
@@ -29,7 +30,7 @@ table(ds$nom_eve)
 1496/(1496+1208)*100#igm denv positive. n = 2704. 55.3% positive
 28/(28+6)*100#pcr denv positive. n = 34. 82.4% positive
 145/(145+70)*100#elisia ns1. n = 215. 67.4% positive.
-ds$acute_testing<-NA
+ds$acute_testing<-0
 ds <- within(ds, acute_testing[(ds$prueba == "ELISA NS1"|ds$prueba == "IgM"|ds$prueba == "PCR"|ds$prueba == "Viral Isolation")& ds$agente=="DENV"] <- 1)
 table(ds$prueba, ds$agente)
 table(ds$acute_testing)
@@ -104,3 +105,77 @@ disease <-  plot_ly() %>%
       titlefont=t, font=f, autosize=T, margin = m)
   
 
+#map the lab confirmed vs unconfirmed and lab pos pos vs negative.
+library(rgdal)
+cases_points<-readOGR("C:/Users/amykr/Google Drive/Kent/james/dissertation/chkv and dengue/data/municipal data/georeferenced cases june 15/geo_direcciones_krystosik_5mayo2016B_latlon.shp")
+
+merged_points<-merge(cases_points, ds, by="CODIGO")
+table(merged_points$DDLat, exclude = NULL)
+table(merged_points$`_merge`, exclude = NULL)
+table(merged_points$acute_testing)
+table(merged_points$confirmed_acute)
+
+#install.packages("labelled")
+library("labelled")
+merged_points$`_merge`<-var_label(merged_points$`_merge`) <- NULL
+merged_points$cases<-1
+merged_points<-merged_points[order(-(grepl('CODIGO', names(merged_points)))+1L)]
+merged_points$DDLat <-as.numeric(as.character(gsub("N", "", merged_points$DDLat)))
+merged_points$DDLon <- as.numeric(as.character(gsub("W", "", merged_points$DDLon)))
+merged_points$DDLon<-merged_points$DDLon*-1    
+
+#geo
+  points<- merged_points
+  library(rgdal)  
+  writeOGR(points, "C:/Users/amykr/Google Drive/Kent/james/dissertation/chkv and dengue/data/municipal data/georeferenced cases june 15", "points", driver = "ESRI Shapefile",overwrite_layer=T)
+  plot(points)
+  points<-as.data.frame(points)
+  points<-points[which(!is.na(points$DDLat))  , ]
+  points<-points[, grepl("CODIGO|Lat|Lon", names(points))]
+  points<-points[order(-(grepl('CODIGO', names(points)))+1L)]
+  write.table(points,file="C:/Users/amykr/Google Drive/Kent/james/dissertation/chkv and dengue/data/municipal data/georeferenced cases june 15/points.geo", quote = F, row.names = F)
+#tested
+  acute_testing_yes<- merged_points[which((merged_points$acute_testing==1))  , ]
+  writeOGR(acute_testing_yes, "C:/Users/amykr/Google Drive/Kent/james/dissertation/chkv and dengue/data/municipal data/georeferenced cases june 15", "acute_testing_yes", driver = "ESRI Shapefile")
+  acute_testing_yes.cas<-  as.data.frame(acute_testing_yes)
+  acute_testing_yes.cas<-acute_testing_yes.cas[, grepl("CODIGO|cases|start_of_week", names(acute_testing_yes.cas))]
+  acute_testing_yes.cas<-acute_testing_yes.cas[order(-(grepl('case', names(acute_testing_yes.cas)))+1L)]
+  acute_testing_yes.cas<-acute_testing_yes.cas[order(-(grepl('CODIGO', names(acute_testing_yes.cas)))+1L)]
+  write.table(acute_testing_yes.cas,file="C:/Users/amykr/Google Drive/Kent/james/dissertation/chkv and dengue/data/municipal data/georeferenced cases june 15/acute_testing_yes.cas", quote = F, row.names = F)
+  
+
+  acute_testing_no<- merged_points[which((merged_points$acute_testing==0))  , ]
+  writeOGR(acute_testing_no, "C:/Users/amykr/Google Drive/Kent/james/dissertation/chkv and dengue/data/municipal data/georeferenced cases june 15", "acute_testing_no", driver = "ESRI Shapefile")
+  acute_testing_no.ctl<-  as.data.frame(acute_testing_no)
+  acute_testing_no.ctl<-acute_testing_no.ctl[, grepl("CODIGO|cases|start_of_week", names(acute_testing_no.ctl))]
+  acute_testing_no.ctl<-acute_testing_no.ctl[order(-(grepl('case', names(acute_testing_no.ctl)))+1L)]
+  acute_testing_no.ctl<-acute_testing_no.ctl[order(-(grepl('CODIGO', names(acute_testing_no.ctl)))+1L)]
+  write.table(acute_testing_no.ctl,file="C:/Users/amykr/Google Drive/Kent/james/dissertation/chkv and dengue/data/municipal data/georeferenced cases june 15/acute_testing_no.ctl", quote = F, row.names = F)
+  
+  table(acute_testing_no$start_of_week)
+  #confirmed acute cases
+  confirmed_acute_pos<- merged_points[which((merged_points$confirmed_acute=="Confirmed +"))  , ]
+  writeOGR(confirmed_acute_pos, "C:/Users/amykr/Google Drive/Kent/james/dissertation/chkv and dengue/data/municipal data/georeferenced cases june 15", "confirmed_acute_pos", driver = "ESRI Shapefile")
+  confirmed_acute_pos.cas<-  as.data.frame(confirmed_acute_pos)
+  confirmed_acute_pos.cas<-confirmed_acute_pos.cas[, grepl("CODIGO|cases|start_of_week", names(confirmed_acute_pos.cas))]
+  write.table(confirmed_acute_pos.cas,file="C:/Users/amykr/Google Drive/Kent/james/dissertation/chkv and dengue/data/municipal data/georeferenced cases june 15/confirmed_acute_pos.cas", quote = F, row.names = F, col.names = F)
+  
+  confirmed_acute_neg<- merged_points[which((merged_points$confirmed_acute=="Confirmed -"))  , ]
+  writeOGR(confirmed_acute_neg, "C:/Users/amykr/Google Drive/Kent/james/dissertation/chkv and dengue/data/municipal data/georeferenced cases june 15", "confirmed_acute_neg", driver = "ESRI Shapefile")
+  confirmed_acute_neg.ctl<-  as.data.frame(confirmed_acute_neg)
+  confirmed_acute_neg.ctl<-confirmed_acute_neg.ctl[, grepl("CODIGO|cases|start_of_week", names(confirmed_acute_neg.ctl))]
+  write.table(confirmed_acute_neg.ctl,file="C:/Users/amykr/Google Drive/Kent/james/dissertation/chkv and dengue/data/municipal data/georeferenced cases june 15/confirmed_acute_neg.ctl", quote = F, row.names = F, col.names = F)
+
+
+
+plot(confirmed_acute_pos)
+plot(confirmed_acute_neg)
+plot(acute_testing_no)
+plot(acute_testing_yes)
+
+#install.packages("rsatscan")
+library("rsatscan")
+  ss.options("C:/Users/amykr/Google Drive/Kent/james/dissertation/chkv and dengue/data/municipal data/georeferenced cases june 15","confirmed_acute_pos")
+  write.ss.prm("C:/Users/amykr/Google Drive/Kent/james/dissertation/chkv and dengue/data/municipal data/georeferenced cases june 15","cali_cases")
+  satscan("C:/Program Files/SaTScan/SaTScan")
+  summary(satscanresult)
